8d2bee7cdc31e4cc4e7319a9061ba9f239aff96c
bzcheeseman <alachapelle@uchicago.edu>
2019-05-18 16:31:25 -0600

    - Fix build issues by providing full paths

--- swagger-go.sh
+++ swagger-go.sh
@@ -2,11 +2,13 @@
 # go look at, https://quay.io/repository/goswagger/swagger?tab=tags
 docker pull quay.io/goswagger/swagger:0.15.0
 
+DOCKER_WORKDIR=/go/src/github.com/notasecret/peacemakr-go-sdk
+
 swagger() {
 	echo "running swagger command with args: $@"
 	ID=`id -u`
 	GID=`id -g`
-	docker run --rm -it -e GOPATH="$(pwd):/go" -u="$ID:$GID" -v $HOME:$HOME -w $(pwd) quay.io/goswagger/swagger:0.15.0 "$@"
+	docker run --rm -it -e GOPATH="/go" -u="$ID:$GID" -v $(pwd):${DOCKER_WORKDIR} -w ${DOCKER_WORKDIR} quay.io/goswagger/swagger:0.15.0 "$@"
 }
 
 swagger "$@"
